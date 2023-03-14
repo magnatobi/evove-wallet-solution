be62ee022721f0d55c1a6093ac628a7a508df866
magnawealth <tobiwealth.me@gmail.com>
2023-03-14 23:42:58 +0100

    IUserAccountRepository added

--- evoveWallet.Data/Interfaces/IUserAccountRepository.cs
+++ evoveWallet.Data/Interfaces/IUserAccountRepository.cs
@@ -1,9 +1,17 @@
-using System;
+using System.Collections.Generic;
+using evoveWallet.Data.Models
 
 namespace evoveWallet.Data.Interfaces
 {
     public interface IUserAccountRepository
     {
-        
+        bool SaveChanges();
+
+        IEnumerable<UserAccount> GetAllUserAccounts();
+        UserAccount GetUserAccountById(int id);
+        void CreateUserAccount(UserAccount userAccount);
+        void UpdateUserAccount(UserAccount userAccount);
+        void DeleteUserAccount(UserAccount userAccount);
+                
     }
 }
